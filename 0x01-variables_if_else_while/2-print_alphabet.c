6a2b577818221fdabadd4c032d443725c642e14e
bosiboriharriet <harrietbosibori3@gmail.com>
2022-07-09 13:22:27 -0700

    e

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,13 @@
  */
 int main(void)
 {
-	int num;
+	int num = 1;
 	char ch ='a';
-	while (ch <= 'z')
+	while (ch <= 'z', num <= 26)
 	{
 		putchar(ch);
 		ch++;
+		num++;
 	}
 	putchar('\n');
 	return 0;
