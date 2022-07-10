d8763f19adabd1792da422da4f67c836a563c571
bosiboriharriet <harrietbosibori3@gmail.com>
2022-07-10 02:48:11 -0700

    f

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 1;
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		--ch;
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
