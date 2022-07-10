bd44ca5dea0a373f625a965e5fe3ccda2d7ad1d2
bosiboriharriet <harrietbosibori3@gmail.com>
2022-07-10 01:32:43 -0700

    f

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 1;
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' or ch == 'q')
+				continue;
+	}
+	{
+		putchar(ch);
+		ch++;
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
+
