1524e2b2c1fba1424b5b259da36d5d9ff2ade55b
bosiboriharriet <harrietbosibori3@gmail.com>
2022-07-10 04:47:46 -0700

    d

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	int lett = 'a';
+	while (lett <= 'f')
+	{
+		putchar(lett);
+		lett++
+	}
+	putchar('\n');
+	return (0);
+}
