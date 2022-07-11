f3561261dacc58a9d83cf4a81124ecc0679d9dd2
bosiboriharriet <harrietbosibori3@gmail.com>
2022-07-10 23:53:40 -0700

    k

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int i = 0;
+
+	putchar((i % 10) + '_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
