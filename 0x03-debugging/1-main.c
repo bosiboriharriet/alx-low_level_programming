4efc35f175d49e6c0e4419cdddf40484717a85c6
bosiboriharriet <99307013+bosiboriharriet@users.noreply.github.com>
2022-07-16 12:24:24 +0300

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -14,11 +14,11 @@ int main(void)
 	i = 0;
 
 	/* while (i < 10) */
-	{
-		putchar(i);
-	}
+	/* { */
+	/*putchar(i); */
+	/* } */
 
-	printf("Infinite loop avoided! \\0/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
