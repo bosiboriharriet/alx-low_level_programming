93f184d7d1bdfa849433d08430566f6ee36a7c8d
bosiboriharriet <99307013+bosiboriharriet@users.noreply.github.com>
2022-07-18 19:08:02 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,16 +1,15 @@
-#includde "main.h"
-
+#include "main.h"
 /**
- * _strchr - point of entry
- * @s: first input
- * @c: second input
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
  * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	for (; s[i] >= '\0'; i ++)
+	for (; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 			return (&s[i]);
