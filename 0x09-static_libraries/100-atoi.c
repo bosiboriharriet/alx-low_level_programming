fd7a1f487b1cf7591e9bdd63dce89e8667adb372
bosiboriharriet <99307013+bosiboriharriet@users.noreply.github.com>
2022-07-27 23:09:39 +0300

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,31 +1,30 @@
 #include "main.h"
+
 /**
- * _strncat - concatenates two strings
- * @dest: input parameter string
- * @src: input parameter string
- * @n: times to iterations
+ * _atoi - convert a string to an integer.
+ * @s: the string to be comverted.
  *
- * Return: dest
+ *Return: The integer value of the comverted string.
  */
-char *_strncat(char *dest, char *src, int n)
+
+int _atoi(char *s)
 {
-	int a;
-	int b;
 
-	a = 0;
+	int sign = 1;
+	unsigned int num = 0;
 
-	while (dest[a] != 0)
-	{
-		a++;
-	}
+	do {
 
-	b = 0;
+		if (*s == '-')
+			sign *= -1;
 
-	while (src[b] != 0 && b < n)
-	{
-		dest[a] = src[b];
-		a++;
-		b++;
-	}
-	return (dest);
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
 }
