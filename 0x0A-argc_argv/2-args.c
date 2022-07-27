5a464d1331d50b97d0ec5dd4859c72e29d0bbaa3
bosiboriharriet <99307013+bosiboriharriet@users.noreply.github.com>
2022-07-28 00:15:10 +0300

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,56 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - prints the function.
- * @argc: counts the arguments of the function.
- * @argv: gives the value of the arguments passed to the function.
+ * @argc: is argument counter to an int.
+ * @argv: is an agument value determiner for char.
  *
- * Return: Return always success.
+ * Return: Returns always success.
 **/
 
 int main(int argc, char *argv[])
 {
-int cents, coins = 0;
+int i;
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
 
-if (argc == 2)
-{
-cents = atoi(*(argv + 1));
-while (cents > 0)
-{
-if (cents % 25 < cents)
-{
-cents -= 25;
-coins++;
-}
-else if (cents % 10 < cents)
-{
-cents -= 10;
-coins++;
-}
-else if (cents % 5 < cents)
-{
-cents -= 5;
-coins++;
-}
-else if (cents % 2 < cents)
-{
-cents -= 2;
-coins++;			}
-else if (cents % 1 < cents)
-{
-cents -= 1;
-coins++;
-}
-}
-}
-else
-{
-printf("Error\n");
-return (1);
-}
-printf("%d\n", coins);
 return (0);
-}
 
+}
 
