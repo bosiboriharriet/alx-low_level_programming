b7b826da28b15cae68b8175ba3cdc6754517a427
bosiboriharriet <99307013+bosiboriharriet@users.noreply.github.com>
2022-08-02 15:42:56 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,23 @@
-#include "dog.h"
-#include <stdlib.h>
-
+#ifndef _DOG_H_
+#define _DOG_H_
 /**
- * free_dog - function to free memory
- * @d: is pointer to dog_t
- *
- * Return: nothing.
+ * struct dog -Dog data.
+ * @name: Name of the dog
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ * Desctiption: a structure with the name, age and owner of a dog.
 **/
 
-void free_dog(dog_t *d)
+typedef struct dog
 {
-if (d != NULL)
-{
-free(d->name);
-free(d->owner);
-free(d);
-}
-}
+char *name;
+char *owner;
+float age;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+#endif
